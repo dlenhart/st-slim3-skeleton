676bdd08e53074ba867909ddd0b76a787dc88a3c
Drew D. Lenhart <drewlenhart@Drews-Mac-mini.fios-router.home>
2018-08-24 09:47:53 -0400

    Adding tests.

---
+++ tests/Test.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class HomepageTest
+ * @package APP\Tests
+ */
+ class Test extends TestCase
+ {
+     public function testGet() {
+         $this->assertTrue(true);
+     }
+ }
